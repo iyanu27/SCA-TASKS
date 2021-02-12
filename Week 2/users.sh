563a1eee2f122773d1c81ad158f23287fb4c9fdd
iyanu27 <57386428+iyanu27@users.noreply.github.com>
2021-02-12 06:23:45 -0800

    this script helps to know the average age of users

--- Week 2/users.sh
+++ Week 2/users.sh
@@ -1,1 +1,21 @@
 
+#!/bin/bash
+output=users.txt
+cat $output
+grep 'M' users.txt
+
+echo
+grep 'F' users.txt
+
+echo
+
+awk '{
+        age[$1]+=$3  #accumulate  total age for this gender
+        counter[$1]+=1  #increment  count of record for this gender female or m$
+        }
+         END {
+                for (gender in age)
+                 {
+                        print gender, age[gender], age[gender]/counter[gender]
+                 }
+                }' users.txt
