dc68b816515c32f61ac353b150aa4df24643ecbd
Ubuntu <Iyanu@IyanuVM.g3y5fqnny5aunjfyvmzhpguaxg.ax.internal.cloudapp.net>
2021-02-19 16:31:29 +0000

    This is a dockerized app

---
+++ Recordapp/docker-compose/mysql/init_db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `member` (
+  `mem_id` int(11) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `member` (mem_id,firstname,lastname ) VALUES(1,Ade,Sule),(2,Fola,Ojo);
+
